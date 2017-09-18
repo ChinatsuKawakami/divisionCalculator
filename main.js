d31b7823c0139d10df4cd817faba06c0da835146
ChinatsuKawakami <ckawakam@my.centennialcollege.ca>
2017-09-18 12:01:01 -0400

    0.0.2 changed color of bottun in css file.

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 
+/*
+Date: 2017-09-18
+Modified Date: 2017-09-18
+Author: Chinatsu Kawakami
+Description: This is the javascript file for calculator for division
+
+*/
+
+/*
 (function(){
 	
 
 
 })
 
-)}();
+)}();*/
