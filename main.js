fa703c143e477080df4ee0a082c3b2aeb2612786
ChinatsuKawakami <ckawakam@my.centennialcollege.ca>
2017-09-18 12:12:47 -0400

    0.0.3 add the Event "this.select();" in main.js file.

--- main.js
+++ main.js
@@ -4,14 +4,23 @@ Date: 2017-09-18
 Modified Date: 2017-09-18
 Author: Chinatsu Kawakami
 Description: This is the javascript file for calculator for division
-
 */
 
-/*
 (function(){
-	
+	'use strict';
 
+	var priceForm=document.getElementById('price');
+    var numForm=document.getElementById('num');
+    var btn =document.getElementById('btn');
+    var result=document.getElementById('result');
 
-})
+    priceForm.addEventListener('click',function(){
+       
+       this.select();
+         
+       });
 
-)}();*/
+    numForm.addEventListener('click',function(){
+       this.select();
+    });
+})();
