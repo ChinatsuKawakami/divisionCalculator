4a8437140c8569716668042b252caf61d8384039
ChinatsuKawakami <ckawakam@my.centennialcollege.ca>
2017-09-18 12:43:03 -0400

    0.0.6 add the Event to show the message if the price the user enters cannot be divided by the number of people accurately in main.js file.

--- main.js
+++ main.js
@@ -27,18 +27,27 @@ Description: This is the javascript file for calculator for division
     btn.addEventListener('click',function(){
        var price = priceForm.value;
        var num = numForm.value;
-       
+       var x1,x2,y1,y2;
+       var unit = 100;
        if(price.match(/^[1-9][0-9]*$/)){
        	//ok
         //result.innerHTML = 'OK';
         if((price%num)===0)
         {
         	result.innerHTML = price + '/' + num +'=' +(price/num);
-        }
+        
        }else{
        	//error
-       	result.innerHTML='Error';
-       }
+       	//result.innerHTML='Error';
+       	x1=Math.floor(price/num/unit)*unit;
+       	y1=price-(x1*num);
+       	x2=Math.ceil(price/num/unit)*unit;
+       	y2=Math.abs(price-(x2*num));
+       	result.innerHTML = 'if one has' +x1+','+y1+'is needed more. <br>'+
+       	'if one has' + x2 + ','+y2+'left over';
 
+       }
+   }
+    
     });
 })();
